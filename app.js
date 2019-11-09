34a45e91a1ad8e8e42779b4f887af04daff98a57
Arvid van de Crommert <arvidvdc@gmail.com>
2019-11-09 12:53:28 +0100

    Items zijn van de lijst en uit de database te verwijderen

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const   express     = require("express"),
         bodyParser  = require("body-parser"),
         seedDB      = require("./seeds"),
         Todo        = require("./models/todo.js"),
-        mongoose    = require("mongoose");
+        mongoose    = require("mongoose"),
+		methodOverride  = require("method-override");
 
 // Express variables
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 // Database connection
 mongoose.connect("mongodb://localhost:27017/todo", { useNewUrlParser: true, useUnifiedTopology: true});
@@ -21,7 +23,7 @@ mongoose.connect("mongodb://localhost:27017/todo", { useNewUrlParser: true, useU
 app.get("/", (req,res)=>{
     Todo.find({}).sort("priority").exec((err,todos)=> {
         if(err) {
-            console.log("Route '/' error: " +err)
+            console.log("Route '/' error: " + err)
         } else {
             res.render("home", { todos: todos });
         };
@@ -29,15 +31,36 @@ app.get("/", (req,res)=>{
 });
 
 // New
-app.get("/new", (req,res)=>{
+app.get("/new", (req,res)=> {
     res.render("new");
 });
 
 // Create
-app.post("/", (req,res)=>{
+app.post("/", (req,res)=> {
     Todo.create(req.body.newTodo, (err,newTodo)=>{
         if(err){
-            console.log("Create Todo error: " +err);
+            console.log("Create Todo error: " + err);
+        } else {
+            res.redirect("/");
+        };
+    });
+});
+
+// Delete
+app.get("/delete/:id", (req,res)=> {
+    Todo.findById(req.params.id, (err,item)=>{
+        if(err){
+            console.log("Find ID to delete error: " + err);
+        } else {
+            res.render("delete", {item: item});
+        };
+    });
+});
+
+app.delete("/:id", (req,res)=> {
+    Todo.findByIdAndDelete(req.params.id, (err,deletedItem)=>{
+        if(err) {
+            console.log("Delete Todo error" + err);
         } else {
             res.redirect("/");
         };
@@ -45,4 +68,4 @@ app.post("/", (req,res)=>{
 });
 
 // listener
-app.listen(3000, ()=>console.log("ToDo started on port 3000"));
+app.listen(3001, ()=>console.log("ToDo started on port 3001"));
--- public/js/todo.js
+++ public/js/todo.js
@@ -5,3 +5,32 @@ $(".fa-plus-circle").click(function() {
 $(".fa-undo").click(function() {
     window.location.href = "/";
 });
+
+$("ul").on("click", "span", function(){
+    $(this).toggleClass("completed");
+});
+
+$(".fa-trash-alt").click(function() {
+    let url = "/delete/" + $(this).parent().attr("id");
+    window.location.href = url;
+});
+
+
+
+
+// $("ul").on("dclick", "span", function(){
+//      $.ajax({
+//         url:    "/",
+//         method: "DELETE"
+//     }).done(()=>{
+//         console.log("Deleted");
+//     });
+//     // $(this).parent().fadeOut(500,function() {
+//         $(this).parent().remove();
+//     // });
+    
+//     let id = $(this).attr("id");
+//     // window.location.href = "/" +id +"?_method='DELETE'";
+
+//     event.stopPropagation();
+// });
