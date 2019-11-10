1d99feab3b67d2fc674c93cfba502416f2e7f6e1
Arvid van de Crommert <arvidvdc@gmail.com>
2019-11-10 08:14:04 +0100

    Owner -user- information added to todoschema

--- models/todo.js
+++ models/todo.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const todoSchema = new mongoose.Schema({
     item: String,
     priority: Number,
-    description: String
+    description: String,
+    owner: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
